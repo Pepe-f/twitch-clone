7a9d3c5c1aa4a7065b88f893a744e0d3232ce838
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-01-20 02:05:13 +0300

    fix: hydration error

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { ReactNode } from "react";
+import { useIsClient } from "usehooks-ts";
+
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
   children: ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const isClient = useIsClient();
   const { collapsed } = useSidebar((state) => state);
+
+  if (!isClient) {
+    return (
+      <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
+  }
 
   return (
     <aside
